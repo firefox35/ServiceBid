a99ea1de79a5a4e1a7b96bdbf3e496566cfdd55f
firefox35 <f1firefox@yahoo.ie>
2016-11-06 14:33:15 +0000

    Installed bootstrap and add a navigation bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,15 @@
 Rails.application.routes.draw do
-  
-  
-  
+
+  root 'pages#home'
  
-
-  get 'pages/home'
-
-  get 'pages/about'
-
-  get 'pages/contact'
+  get 'about' => "pages#about", as: :about
+  get 'contact' => "pages#contact", as: :contact
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'pages#home'
-
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
