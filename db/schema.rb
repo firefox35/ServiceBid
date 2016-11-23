056cf63ca1ab1b223ef6a668dfa0ba62b1600faa
firefox35 <f1firefox@yahoo.ie>
2016-11-23 22:15:19 +0000

    rollback on database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161123211859) do
+ActiveRecord::Schema.define(version: 20161122202742) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -72,6 +72,8 @@ ActiveRecord::Schema.define(version: 20161123211859) do
     t.datetime "updated_at",                          null: false
     t.string   "address"
     t.integer  "phone"
+    t.string   "name"
+    t.string   "dealer_name"
   end
 
   add_index "dealers", ["email"], name: "index_dealers_on_email", unique: true
